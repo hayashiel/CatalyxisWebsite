b4744dac3814b428d2d73c9d5c7263b067ea402e
hayashiel <47303759+hayashiel@users.noreply.github.com>
2024-12-29 20:30:58 +0000

    Testing out react-bootstrap

--- catalyxis-website/src/App.js
+++ catalyxis-website/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link, View, NotFoundBoundary, useLoadingRoute } from 'react-navi';
 import { MDXProvider } from '@mdx-js/react';
 import './App.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   let loadingRoute = useLoadingRoute()
