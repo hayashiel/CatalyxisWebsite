9bb8123b297835edb48ef662ac3d5695d35a4a39
hayashiel <47303759+hayashiel@users.noreply.github.com>
2024-12-31 00:31:23 +0000

    test

--- catalyxis-website/src/index.js
+++ catalyxis-website/src/index.js
@@ -1,7 +1,7 @@
 import {React} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'App.js';
-
+import App from 'App';
+console.log("Hello");
 const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.render(App, document.getElementById('root'));
